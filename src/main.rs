23860c5114da92c762ba077ee22c914fea2112ec
Martin Olivier <martin.olivier@live.fr>
2022-10-06 15:46:23 +0200

    feat: new ui
    Signed-off-by: Martin Olivier <martin.olivier@live.fr>

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod globals;
 mod ui;
 
 fn main() {
+    sudo::escalate_if_needed().unwrap();
     let application = Application::builder()
         .application_id("com.martin-olivier.airgorah")
         .build();
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,24 +1,49 @@
 mod dialog;
 mod interface;
-mod progress;
 mod scan;
 
 use crate::globals::IFACE;
 use dialog::ErrorDialog;
 use gtk4::prelude::*;
-use gtk4::{
-    AboutDialog, Application, ApplicationWindow, Box, Button, CellRendererText, ListStore,
-    Orientation, TreeView, TreeViewColumn,
-};
+use gtk4::*;
 use regex::Regex;
-use std::io::Read;
 use std::rc::Rc;
 
-fn build_aps_view(view: &TreeView) {
+fn build_aps_model() -> ListStore {
+    let model = ListStore::new(&[
+        glib::Type::STRING,
+        glib::Type::STRING,
+        glib::Type::STRING,
+        glib::Type::STRING,
+        glib::Type::STRING,
+        glib::Type::STRING,
+        glib::Type::STRING,
+    ]);
+
+    // fake values
+    let it = model.append();
+    model.set(
+        &it,
+        &[
+            (0, &"NEUF_A598".to_string()),
+            (1, &"10:20:30:40:50".to_string()),
+            (2, &"2.4 GHz".to_string()),
+            (3, &"6".to_string()),
+            (4, &"55".to_string()),
+            (5, &"95".to_string()),
+            (6, &"WPA2".to_string()),
+        ],
+    );
+    // fake values END
+    model
+}
+
+fn build_aps_view() -> TreeView {
+    let view = TreeView::new();
     let colomn_names = [
         "ESSID",
         "BSSID",
-        "Vendor",
+        "Band",
         "Channel",
         "Speed",
         "Power",
@@ -40,14 +65,37 @@ fn build_aps_view(view: &TreeView) {
         column.add_attribute(&renderer, "text", pos);
         pos += 1;
     }
+    view
+    //renderer2.set_background(Some("Orange"));
+}
+
+//
+
+fn build_cli_model() -> ListStore {
+    let model = ListStore::new(&[glib::Type::STRING]);
+
+    // fake values
+    let it = model.append();
+    model.set(&it, &[(0, &"01:21:32:45:22".to_string())]);
+    // fake values END
+    model
+}
+
+fn build_cli_view() -> TreeView {
+    let view = TreeView::new();
+
+    let column = TreeViewColumn::builder().title("Clients").build();
+    view.append_column(&column);
+
+    let renderer = CellRendererText::new();
+    column.pack_start(&renderer, true);
+    column.add_attribute(&renderer, "text", 0);
+
+    view
     //renderer2.set_background(Some("Orange"));
 }
 
 pub fn build_ui(app: &Application) {
-    sudo::escalate_if_needed().unwrap();
-
-    let scan_window = Rc::new(scan::ScanWindow::new());
-
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Airgorah")
@@ -55,38 +103,101 @@ pub fn build_ui(app: &Application) {
         .default_height(370)
         .build();
 
-    let main_box = Box::new(Orientation::Vertical, 10);
+    let aps_model = build_aps_model();
+    let aps_view = build_aps_view();
+
+    aps_view.set_vexpand(true);
+    aps_view.set_hexpand(true);
+    aps_view.set_model(Some(&aps_model));
+
+    let cli_model = build_cli_model();
+    let cli_view = build_cli_view();
+
+    cli_view.set_vexpand(true);
+    cli_view.set_hexpand(true);
+    cli_view.set_model(Some(&cli_model));
+
+    // SCAN
+
+    let scan_box = Box::new(Orientation::Vertical, 10);
+
+    let ghz_2_4_but = CheckButton::builder().active(true).label("2.4 GHZ").build();
+    let ghz_5_but = CheckButton::builder().active(false).label("5 GHZ").build();
+
+    ghz_2_4_but.set_margin_start(6);
+    ghz_5_but.set_margin_end(6);
+
     let but_box = Box::new(Orientation::Horizontal, 10);
+    but_box.append(&ghz_2_4_but);
+    but_box.append(&ghz_5_but);
 
-    let model = Rc::new(ListStore::new(&[
-        glib::Type::STRING,
-        glib::Type::STRING,
-        glib::Type::STRING,
-        glib::Type::STRING,
-        glib::Type::STRING,
-        glib::Type::STRING,
-        glib::Type::STRING,
-    ]));
+    let band_frame = Frame::new(Some("Band"));
+    band_frame.set_child(Some(&but_box));
 
-    let it = model.append();
-    model.set(
-        &it,
-        &[
-            (0, &"NEUF_A598".to_string()),
-            (1, &"10:20:30:40:50".to_string()),
-            (2, &"APPLE INC".to_string()),
-            (3, &"6".to_string()),
-            (4, &"55".to_string()),
-            (5, &"95".to_string()),
-            (6, &"WPA2".to_string()),
-        ],
+    let channel_filter_entry = Rc::new(
+        Entry::builder()
+            .placeholder_text("ex: 1,6,11")
+            .sensitive(false)
+            .build(),
     );
+    let channel_filter_but = CheckButton::builder().active(false).build();
+    channel_filter_but.set_margin_start(10);
 
-    let view = TreeView::new();
-    build_aps_view(&view);
+    let channel_filter_entry_ref = channel_filter_entry.clone();
+    channel_filter_but.connect_toggled(move |this| {
+        match this.is_active() {
+            true => channel_filter_entry_ref.set_sensitive(true),
+            false => channel_filter_entry_ref.set_sensitive(false),
+        };
+    });
 
-    view.set_vexpand(true);
-    view.set_model(Some(&*model));
+    let bssid_filter_entry = Rc::new(
+        Entry::builder()
+            .placeholder_text("ex: 10:20:30:40:50")
+            .sensitive(false)
+            .build(),
+    );
+    let bssid_filter_but = CheckButton::builder().active(false).build();
+    bssid_filter_but.set_margin_start(10);
+
+    let bssid_filter_entry_other = bssid_filter_entry.clone();
+    bssid_filter_but.connect_toggled(move |this| {
+        match this.is_active() {
+            true => bssid_filter_entry_other.set_sensitive(true),
+            false => bssid_filter_entry_other.set_sensitive(false),
+        };
+    });
+
+    let channel_box = Box::new(Orientation::Horizontal, 10);
+    channel_box.append(&channel_filter_but);
+    channel_box.append(channel_filter_entry.as_ref());
+
+    let bssid_box = Box::new(Orientation::Horizontal, 10);
+    bssid_box.append(&bssid_filter_but);
+    bssid_box.append(bssid_filter_entry.as_ref());
+
+    let channel_frame = Frame::new(Some("Channel filter"));
+    channel_frame.set_child(Some(&channel_box));
+
+    let bssid_frame = Frame::new(Some("BSSID filter"));
+    bssid_frame.set_child(Some(&bssid_box));
+
+    let scan_but = Button::with_label("Apply");
+
+    let deauth_button = Button::with_label("Deauth attack");
+
+    scan_box.append(&band_frame);
+    scan_box.append(&channel_frame);
+    scan_box.append(&bssid_frame);
+    scan_box.append(&scan_but);
+    scan_box.append(&deauth_button);
+
+    scan_box.set_hexpand(false);
+
+    scan_box.set_margin_top(10);
+    scan_box.set_margin_end(10);
+
+    //
 
     let about_button = Button::with_label("About");
     about_button.connect_clicked(|_| {
@@ -94,22 +205,15 @@ pub fn build_ui(app: &Application) {
         about.show();
     });
 
-    let scan_button = Button::with_label("Scan");
+    let main_box = Box::new(Orientation::Horizontal, 10);
 
-    let scan_window_ref = scan_window.clone();
-    scan_button.connect_clicked(move |_| {
-        scan_window_ref.window.show();
-    });
+    let panned = Paned::new(Orientation::Vertical);
+    panned.set_wide_handle(true);
+    panned.set_start_child(Some(&aps_view));
+    panned.set_end_child(Some(&cli_view));
 
-    main_box.append(&view);
-    main_box.append(&but_box);
-
-    but_box.append(&about_button);
-    but_box.append(&scan_button);
-
-    but_box.set_margin_end(10);
-    but_box.set_margin_start(10);
-    but_box.set_margin_bottom(10);
+    main_box.append(&panned);
+    main_box.append(&scan_box);
 
     window.set_child(Some(&main_box));
     window.show();
@@ -142,7 +246,7 @@ pub fn build_ui(app: &Application) {
         };
     });
 
-    let scan_window_ref = scan_window.clone();
+    /*let scan_window_ref = scan_window.clone();
     scan_window.scan_but.connect_clicked(move |_| {
         // return if no IFACE
         if !scan_window_ref.ghz_2_4_but.is_active() && !scan_window_ref.ghz_5_but.is_active() {
@@ -235,5 +339,13 @@ pub fn build_ui(app: &Application) {
             println!("done");
         });
         prog_win.window.show();
-    });
+    });*/
+
+    // Note:
+    /*
+    faire un scan en continu /tmp/scan et lire le fichier grace à un glib::timer toute les 1 sec
+    mettre les flitres de scan à droite
+    Start/stop scan
+    Clear
+    */
 }
